ec3d2c93feef1db5fd0fb5db1a3106321f6c8d82
Mahmudjon <mahmudjonrahimjonov007@gmail.com>
2025-10-05 15:32:28 +0500

    JS_DOM

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-const btns = document.querySelectorAll('.btn') 
-const dom_items = document.querySelectorAll('.dom_item') 
-btns.forEach(btn =>{
-    btn.addEventListener('click', ()=>{ 
-        const color = btn.dataset.color
+alert("Сночала нажмите на цвет а потом можете игратся с фигурками :)")
 
-        dom_items.forEach(dom_item =>{
-            dom_item.style.backgroundColor = color
-        })
-       })   
-    })
+const btns = document.querySelectorAll(".btn");
+const figures = document.querySelectorAll(".btn_figure");
+const dom_items = document.querySelectorAll(".dom_item");
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const color = btn.dataset.color;
 
-// console.log(dom_items);
-// const computedStyle = getComputedStyle(btn);
-        // const color = computedStyle.backgroundColor;
-
-        // if (btn.textContent === 'x') {
-        //     dom_items.forEach(item => item.style.backgroundColor = '');
-        // } else {
-        //     dom_items.forEach(item => item.style.backgroundColor = color);
-        // }
+    dom_items.forEach((dom_item) => {
+      dom_item.style.backgroundColor = color;
+    });
+  });
+});
+figures.forEach((figure) => {
+  figure.addEventListener("click", () => {
+    const style = figure.dataset.figure;
+    dom_items.forEach((dom_item) => {
+      dom_item.classList.remove(
+        "triangle",
+        "hexagon",
+        "pentagon",
+        "star",
+        "square"
+      );
+      dom_item.classList.add(`${style}`);
+    });
+  });
+});
